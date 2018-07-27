91a421bf26bcaa4077e62ca03020efc985280a09
Peterson <el96choclo@gmail.com>
2018-07-26 19:53:03 -0700

    Add files via upload

---
+++ parallel.sh
@@ -0,0 +1,11 @@
+!/bin/sh
+/*
+	Checks for parallel, or duplicate scripts and prints a message
+	 when the process is done
+
+	the code below is just an example of the printout. Not sure as to how
+	the variable / logic of this particular script should function.
+*/
+echo Instance #1 of script foo.sh is running.
+echo Instance #2 of script foo.sh is running.
+echo Instance #3 of script foo.sh completed without errors!
